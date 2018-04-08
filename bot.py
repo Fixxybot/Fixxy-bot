b7ff14ce0c4c4ea00ec1feab7c1afd7d7d4d02f2
KarlKK <carloscastillo247654@gmail.com>
2018-04-08 18:00:44 +0200

    some changes

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ def restart(bot, update):
 
 
 def start(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text="Hola, soy un bot creado con python, para el grupo del leeco le pro 3 x722.", reply_to_message_id=update.message.message_id)
+	bot.send_message(chat_id=update.message.chat_id, text="*Comandos útiles:*\n\n/aosip - Ultima AOSIP\n\n/Gapps - Link para distintas GAPPS\n\n/gat - Fotos de gatos adorables xd\n\n/notificaciones - Sigue este tutorial si no te llegan notificaciones en EUI\n\n/grupos - Grupos que pueden ser de utilidad\n\n/gcam - ultima camara de google\n\n/selinux - Para cambiar a permisive etc\n\n/roms - Tutorial para instalar ROMS\n\n/logcat - Como hacer un logcat para que se puedan corregir esos errores\n\n/magisk - descargar magisk manager\n\n*SI QUIERES ENVIAR ALGUNA SUGERENCIA, CONTACTA CON* @KarloMoDZz *o* @Gabronog", parse_mode=telegram.ParseMode.MARKDOWN, reply_to_message_id=update.message.message_id)
 
 def mention(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text=random.choice(["QUE QUIERE", "KI TI PASA","Supongo que llevaras razon....", "Estaba claro....", "Me vas a hacer llorar :(", "jaja", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]), reply_to_message_id=update.message.message_id)
@@ -34,32 +34,24 @@ def id(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="ID: "+str(chat_id), reply_to_message_id=update.message.message_id)
 
 def aosip(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text="*Puedes descargar ultima AOSIP pulsando *[AQUÍ](http://get.aosiprom.com/zl1).", parse_mode=telegram.ParseMode.MARKDOWN, reply_to_message_id=update.message.message_id)
-
-def x2(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text="*Puedes descargar la ultima AOSIP para tu Leeco Le Max 2 pulsado *[AQUÍ](https://get.aosiprom.com/x2/)", parse_mode=telegram.ParseMode.MARKDOWN)
+	bot.send_message(chat_id=update.message.chat_id, text="🌐 *LINK PARA ULTIMA AOSIP:*\n├[🗂LEPRO3](http://get.aosiprom.com/zl1)\n└[🗂LEMAX2](http://get.aosiprom.com/x2)", parse_mode=telegram.ParseMode.MARKDOWN, reply_to_message_id=update.message.message_id)
 
 
-
-
-def link(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text='El link de este grupo es: https://t.me/joinchat/EXE9HESRmd4ihACntQbtoA', reply_to_message_id=update.message.message_id)
 
 def gat(bot, update):
-	bot.send_document(chat_id=update.message.chat_id, document=BQADBAADIQYAAmYu6VGU1rcuxJqDlAI )
-
+	bot.send_photo(chat_id=update.message.chat_id, photo=random.choice(['https://cdn2www.mundo.com/fotos/201504/Un-adorable-gato-disfrazado-de-conejo1-600x400.jpg', "http://www.abc.es/media/sociedad/2018/03/10/miau-kJkG--1240x698@abc.jpg", "https://i.pinimg.com/originals/3d/c0/ab/3dc0ab8e387f66f17a180d1bd89af055.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoPyX7KJTVTz1CWdnhKd2hAXIA0XVLIBDkivXONvddGqa5eIwT", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn0klvfUNyZfHR3IBAWaPnpSFLj8be4KOkmxH1auaC_vuMGe3l", "http://www.mujerhoy.com/multimedia/201710/30/media/gatos/12.jpg"]))
 
 def hola(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text=random.choice(["Hola, cuanto tiempo :D", "Hola", "Eyyy", "Hacia mucho tiempo que no te veia", "Holaaa!!! Cuanto tiempo tio?!?!?!?!", "No tengo nada que decir jajaja"]), reply_to_message_id=update.message.message_id)
+	bot.send_message(chat_id=update.message.chat_id, text=random.choice(["Hola, cuanto tiempo :D", "Hola", "Eyyy", "Hacia mucho tiempo que no te veia", "adios", "Holaaa!!! Cuanto tiempo tio?!?!?!?!", "No tengo nada que decir jajaja"]), reply_to_message_id=update.message.message_id)
 
 def gapps(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text="*Estas son las diferentes GAPPS que hay para android 8:* \n\n[MINDTHEGAPPS](http://downloads.codefi.re/jdcteam/javelinanddart/gapps/)\n\n[OPENGAPPS](http://opengapps.org/)\n\n[MICROG](https://microg.org/download.html)",parse_mode=telegram.ParseMode.MARKDOWN)
+	bot.send_message(chat_id=update.message.chat_id, text="🌐*Estas son las diferentes GAPPS que hay para android 8:*\n├[💊OPENGAPPS](http://opengapps.org/)\n|\n├[💊MICROG](https://microg.org/download.html)\n|\n└[💊MindTheGapps](http://downloads.codefi.re/jdcteam/javelinanddart/gapps/)",parse_mode=telegram.ParseMode.MARKDOWN)
 
 def notificaciones(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="*Si tienes problemas con las notificaciones sigue* [ESTE TUTORIAL](http://telegra.ph/NO-PERDER-NOTIFICACIONES-EN-EUI-03-30).", parse_mode=telegram.ParseMode.MARKDOWN)
 
 def grupos(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text="*GRUPOS RELACIONADOS:*\n\n[72x](https://t.me/joinchat/CszTz0YaMuyR3MwBuzy-nw)\n\n[ALMACEN](http://T.me/AlmacenLeEco)\n\n[GCAM](https://t.me/joinchat/AAAAAEa6HOjwRaWAbI8-lQ)\n\n[LeecoIngles](https://t.me/leecolepro3roms)", parse_mode=telegram.ParseMode.MARKDOWN)
+	bot.send_message(chat_id=update.message.chat_id, text="🌐*GRUPOS QUE TE PUEDEN INTERESAR:*\n\n🌐*LE PRO 3*\n├[📱LePro3](https://t.me/joinchat/CszTz0YaMuyR3MwBuzy-nw)\n├[📱LePro3x722](https://t.me/Leecox722)\n└[📱LePro3(Inglés)](https://t.me/leecolepro3roms)\n\n🌐*LE MAX 2*\n├[📱LeMax2AOSIP](https://t.me/AOSiP_x2)\n└[📱LeMax2INTERNACIONAL](https://t.me/lemax2xda)", parse_mode=telegram.ParseMode.MARKDOWN)
 
 def gcam(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="*PUEDES DESCARGAR LA CAMARA DE GOOGLE DESDE *[AQUÍ](https://www.celsoazevedo.com/files/android/google-camera)", parse_mode=telegram.ParseMode.MARKDOWN, reply_to_message_id=update.message.message_id)
@@ -78,9 +70,7 @@ def magisk(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="*PUEDES DESCARGAR MAGISK DESDE *[AQUÍ](http://tiny.cc/latestmagisk)", parse_mode=telegram.ParseMode.MARKDOWN, reply_to_message_id=update.message.message_id)
 
 
-def creador(bot, update):
-	text = update.message.text
-	bot.send_message(chat_id=-1001150392798, text=(text))
+
 
 
 def new_user(bot, update):
@@ -89,17 +79,19 @@ def new_user(bot, update):
 		if not user.is_bot:
 			user_name = user.first_name or user.last_name or user.username
 			user_name = ", {}".format(user_name) if user_name else ""
-			message_texts.append("¡Bienvenido{} al grupo!".format(user_name))
+			message_texts.append("¡*Bienvenid@{} ,al grupo de Leeco!".format(user_name))
 	if message_texts:
 		bot.send_message(chat_id=update.message.chat_id, text='\n'.join(message_texts))
 
+def ancla(bot, update):
+	bot.pin_chat_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
 
 
 
-#COSAS DE PRUEBA QUE DAN ERRORES
 
-# invalid(bot, update):
-#	bot.send_message(chat_id=update.message.chat_id, text="Comando invalido, prueba con otro", reply_to_message_id=update.message.message_id)
+
+
+
 
 
 
@@ -108,8 +100,6 @@ start = CommandHandler("start", start)
 mention = MessageHandler(Filters.entity("mention"), mention)
 botid = CommandHandler("id", id)
 aosip = CommandHandler("aosip", aosip)
-link = CommandHandler("link", link)
-#invalid = MessageHandler(Filters.command, invalid)
 gat = CommandHandler("gat", gat)
 hola = RegexHandler("Hola", hola)
 gapps = CommandHandler("gapps", gapps)
@@ -120,9 +110,8 @@ selinux = CommandHandler("selinux", selinux)
 roms = CommandHandler("roms", roms)
 logcat = CommandHandler("logcat", logcat)
 magisk = CommandHandler("magisk", magisk)
-creador = MessageHandler(Filters.chat(292633884), creador)
-x2 = CommandHandler("x2", x2)
 new_user = MessageHandler(Filters.status_update.new_chat_members, new_user)
+ancla = RegexHandler("Anclado:", ancla)
 
 
 
@@ -138,8 +127,6 @@ dispatcher.add_handler(start)
 dispatcher.add_handler(mention)
 dispatcher.add_handler(botid)
 dispatcher.add_handler(aosip)
-#dispatcher.add_handler(invalid)
-dispatcher.add_handler(link)
 dispatcher.add_handler(gat)
 dispatcher.add_handler(hola)
 dispatcher.add_handler(gapps)
@@ -150,9 +137,9 @@ dispatcher.add_handler(selinux)
 dispatcher.add_handler(roms)
 dispatcher.add_handler(logcat)
 dispatcher.add_handler(magisk)
-dispatcher.add_handler(creador)
-dispatcher.add_handler(x2)
 dispatcher.add_handler(new_user)
+dispatcher.add_handler(ancla)
+
 
 
 
