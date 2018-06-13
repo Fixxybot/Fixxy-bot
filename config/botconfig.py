9d4b19299c25c5ca1f6e337dc7c04974321739bc
gabr0 <gabronogue@gmail.com>
2018-06-13 02:03:21 +0200

    Huge refactor
    Signed-off-by: gabr0 <gabronogue@gmail.com>

---
+++ config/botconfig.py
@@ -0,0 +1,9 @@
+
+
+# the token of the bot you get from t.me/botfather (type: str)
+
+bot_token = "yourtoken"
+
+# a python list containing the telegram ids of the chats the bot is in
+
+CHAT_IDS_ES = [-1001074112167, -1001176122092, "@Leecox722", "@AOSiP_x2"]
