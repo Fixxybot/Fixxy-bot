b229958a9c092f6240e6b58eb53d6217878d5ce1
gabr0 <gabronogue@gmail.com>
2018-06-18 22:05:16 +0200

    Update database

--- lib/Db.py
+++ lib/Db.py
@@ -1,35 +1,58 @@
+import datetime
+
 from peewee import *
 
-
 db = SqliteDatabase('db/bot.db')
-
-
-class Admin(Model):
-    """Admin model."""
-
-    # username = CharField(max_length=255, unique=True)
-    id = IntegerField(unique=True)
-
-    class Meta:
-        """Import db."""
-
-        database = db
 
 
 class User(Model):
     """"User model"""
 
     id = IntegerField(unique=True)
-    warn = IntegerField(unique=False)
     description = TextField(unique=False)
     afk = BooleanField(unique=False)
+    group_admin = BooleanField(default=False)
+    sudo_admin = BooleanField(default=False)
 
     class Meta:
         """"Import db."""
         database = db
 
 
+class Group(Model):
+    """"Group model"""
+
+    group_id = IntegerField(unique=True)
+    lang = CharField(default="en")
+
+    class Meta:
+        """"Import db"""
+        database = db
+
+
+class warns(Model):
+    user = CharField()
+    id = IntegerField()
+    group = IntegerField()
+    last = CharField
+
+    class Meta:
+        """"import db"""
+        database = db
+
+
+class commands(Model):
+    """"Command model db"""
+    id_group = IntegerField(unique=False)
+    command = TextField()
+    text = TextField()
+    date = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        """"import db"""
+        database = db
+
+
 def main():
-    db = SqliteDatabase('db/bot.db')
     db.connect()
-    db.create_tables([Admin, User], safe=True)
+    db.create_tables([User, Group], safe=True)
